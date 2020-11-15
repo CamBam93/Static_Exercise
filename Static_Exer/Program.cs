19d65ba9b38adc6cd1e3518954044b1a63b8a550
unknown <cannicam@gmail.com>
2020-11-15 17:42:57 -0600

    completed

---
+++ Static_Exer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Static_Exer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cels = TempConverter.FahrenhietToCelsius(74);
+            Console.WriteLine($"That temp in Celsius is {cels}");
+            var fahr = TempConverter.CelsiusToFahrenhiet(14);
+            Console.WriteLine($"That temp in fahrenhiet is {fahr}");
+        }
+    }
+}
---
+++ Static_Exer/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Exer
+{
+    public static class TempConverter
+    {
+        public static double FahrenhietToCelsius(double fahrenhiet)
+        {
+            var c = (fahrenhiet - 32) / 1.8;
+            return c;
+        }
+
+        public static double CelsiusToFahrenhiet(double celcius)
+        {
+            var f = (celcius * 9) / 5 + 32;
+            return f;
+        }
+    }
+}
